dd3f0e8baf7c13174971b8037b3ba3f456a48b1e
Zomby Fraser <agf4@njit.edu>
2023-02-06 03:57:11 -0500

    refactor

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 from Teachworks import Teachworks
 
+teachworks = Teachworks()
+
+teachworks.login()
+teachworks.navigate_to_calendar_list()
+sessions = teachworks.get_daily_sessions()
+working_hours = teachworks.calculate_hours(sessions)
+
+wages = (working_hours['test_prep']*teachworks.TEST_PREP_WAGE + working_hours['school']*teachworks.SCHOOL_WAGE)*(1-teachworks.TAX_RATE)
+print(f"You made {str(round(wages,2))} after taxes today!")
