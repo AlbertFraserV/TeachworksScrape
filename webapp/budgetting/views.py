454abf738351fb4e669da9e6dace88b1d0faebe6
Zomby Fraser <agf4@njit.edu>
2023-02-23 18:53:32 -0500

    Calculate daily net spendings and total net earnings

--- webapp/budgetting/views.py
+++ webapp/budgetting/views.py
@@ -3,6 +3,7 @@ from django.template import loader
 from django.shortcuts import render
 from django.urls import reverse
 from django.db.models import Sum
+from django.db.models.functions import Round
 
 from datetime import date
 
@@ -34,10 +35,10 @@ def viewSpendings(request):
 def addSpendings(request):
     new_amount = DailySpent(spent_date=request.POST['spent_date'], spent_amount=request.POST['spent_amount'], spent_name=request.POST['spent_name'])
     new_amount.save()
-    netSpendings(request, date=request.POST['spent_date'])
+    updateNetSpendingDay(date=request.POST['spent_date'])
     return HttpResponseRedirect(reverse('budget:spendings'))
 
-def updateNetSpendingDay(date=date.today()):
+def updateNetSpendingDay():
     # Get unique dates from both Payment and Spending models
     dates_paid = list(DailyPaid.objects.values_list('paid_date', flat=True).distinct())
     dates_spent = list(DailySpent.objects.values_list('spent_date', flat=True).distinct())
@@ -52,3 +53,6 @@ def recalcAllNetSpendings(request):
     all_dates = DailyPaid.objects.values('paid_date').distinct()
     for dates in all_dates:
         updateNetSpendingDay(dates['spent_date'])
+
+def calculateTotalNetEarnings():
+    total = DailyNet.objects.aggregate(total=Round(Sum('daily_net_earnings'),2))
